3a215651e209f9b62f90f57684d2f06696ab2472
Yusuf Karakaya <yusufkarakaya92@gmail.com>
2026-03-23 21:44:09 -0700

    setup local postgress

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,9 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import pool from './db.js'
 import express from 'express'
+
 const app = express()
 
 import healthRoutes from './routes/health.routes.js'
@@ -6,5 +11,15 @@ import healthRoutes from './routes/health.routes.js'
 app.use(express.json())
 
 app.use('/api/health', healthRoutes)
+
+app.get('/users', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users')
+    res.json(result.rows)
+  } catch (error) {
+    console.error('Error executing query', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
 
 export default app
