604678b590ef3028a25b0b70ad39cb450e18e660
Yusuf Karakaya <yusufkarakaya92@gmail.com>
2026-04-16 22:18:47 -0700

    login page designed - global style implemented

--- client/src/auth/login.tsx
+++ client/src/auth/login.tsx
@@ -32,27 +32,60 @@ export const Login: React.FC = () => {
   }
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <button type="submit">Login</button>
-      </form>
+    <div className="min-h-screen justify-center items-center flex flex-col min-w-full bg-gray-100">
+      <div className="w-full max-w-md bg-white rounded-2xl p-6 shadow-lg">
+        <h1 className="text-2xl font-bold text-center mb-6 text-(--text-100) ">
+          Welcome Back
+        </h1>
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div>
+            <label className="text-sm font-medium block mb-1" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="block border  w-full px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-(--accent-200)"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="you@example.com"
+              required
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              className="block border w-full px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-(--accent-200)"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="********"
+              required
+            />
+          </div>
+
+          <button
+            className="bg-(--primary-100) hover:bg-(--primary-200) transition text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-(--primary-200) cursor-pointer w-full"
+            type="submit"
+          >
+            Login bro
+          </button>
+
+          <div>
+            <p className="text-sm text-center">
+              Don't have an account?{' '}
+              <a
+                href="/register"
+                className="text-(--accent-200) hover:underline cursor-pointer"
+              >
+                Register here
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
